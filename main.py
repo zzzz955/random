c3747720fa82386e194f15f0c832ec0b02e7d19f
SangHyeok <zzzzz955@gmail.com>
2023-11-06 14:49:49 +0900

    메인 윈도우 다이얼로그 연동

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QFr
     QPushButton, QGridLayout, QMessageBox, QLineEdit, QHBoxLayout
 from PyQt5.Qt import QIntValidator, Qt
 from qt_material import apply_stylesheet
-import random_ign
+from dialogs import create_Random_IGN
 
 
 class random_IGN_Create(QMainWindow):
@@ -23,12 +23,11 @@ class random_IGN_Create(QMainWindow):
         label3 = QLabel("<b>아랫 자음 선택</b>")
         label4 = QLabel('<b>자릿수 지정 : </b>')
         self.lineedit1 = QLineEdit()
-        self.do_ran = QPushButton('닉네임 추출 시작')
-
-        self.exit_button = QPushButton("종료")
-        self.selected_cho = []
-        self.selected_jung = []
-        self.selected_jong = []
+        self.button1 = QPushButton('닉네임 추출 시작')
+        self.button2 = QPushButton("종료")
+        self.cho_list = []
+        self.jung_list = []
+        self.jong_list = []
 
         # 레이아웃 세팅
         frame1_gridlayout = QGridLayout()
@@ -45,7 +44,7 @@ class random_IGN_Create(QMainWindow):
                 self.cho_button = QPushButton(str(chr(0x1100 + (i * 7 + j))))
                 frame1_gridlayout.addWidget(self.cho_button, i, j)
                 self.cho_button.setCheckable(True)
-                self.selected_cho.append(self.cho_button)
+                self.cho_list.append(self.cho_button)
 
         layout.addWidget(label2)
         layout.addWidget(frame2)
@@ -57,7 +56,7 @@ class random_IGN_Create(QMainWindow):
                 self.jung_button = QPushButton(str(chr(0x1161 + (i * 7 + j))))
                 frame2_gridlayout.addWidget(self.jung_button, i, j)
                 self.jung_button.setCheckable(True)
-                self.selected_jung.append(self.jung_button)
+                self.jung_list.append(self.jung_button)
 
         layout.addWidget(label3)
         layout.addWidget(frame3)
@@ -69,17 +68,17 @@ class random_IGN_Create(QMainWindow):
                 self.jong_button = QPushButton(str(chr(0x11A8 + (i * 7 + j))))
                 frame3_gridlayout.addWidget(self.jong_button, i, j)
                 self.jong_button.setCheckable(True)
-                self.selected_jong.append(self.jong_button)
+                self.jong_list.append(self.jong_button)
 
         layout.addLayout(hlayout1)
         hlayout1.addWidget(label4)
         hlayout1.addWidget(self.lineedit1)
-        hlayout1.addWidget(self.do_ran)
-        layout.addWidget(self.exit_button)
+        hlayout1.addWidget(self.button1)
+        layout.addWidget(self.button2)
 
         # 시그널 세팅
-        self.do_ran.clicked.connect(self.run_ran)
-        self.exit_button.clicked.connect(self.close)
+        self.button1.clicked.connect(self.run_ran)
+        self.button2.clicked.connect(self.close)
 
         # 위젯 제약
         label4.setAlignment(Qt.AlignCenter)
@@ -92,6 +91,21 @@ class random_IGN_Create(QMainWindow):
                 if digit <= 0 or digit > 32:
                     QMessageBox.warning(self, '입력 값 오류', '입력 된 자릿 수를 확인해 주세요.\n'
                                                          '입력 가능한 자릿 수 : 1 ~ 32')
+                else:
+                    selected_cho_list = []
+                    selected_jung_list = []
+                    selected_jong_list = []
+                    for button in self.cho_list:
+                        if button.isChecked():
+                            selected_cho_list.append(button.text())
+                    for button in self.jung_list:
+                        if button.isChecked():
+                            selected_jung_list.append(button.text())
+                    for button in self.jong_list:
+                        if button.isChecked():
+                            selected_jong_list.append(button.text())
+                    show_dialog = create_Random_IGN(selected_cho_list, selected_jung_list, selected_jong_list)
+                    show_dialog.exec()
         except Exception as e:
             print(e)
 
